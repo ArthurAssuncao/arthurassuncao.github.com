7d10e67130ce071097349ab1ba5bafd436fa0d2b
ArthurAssuncao <arthur.xx92@gmail.com>
2013-04-20 15:45:53 -0300

    atualizado: para nova versao do github portfolio generator

--- github_portfolio_generator/github_portfolio_generator.js
+++ github_portfolio_generator/github_portfolio_generator.js
@@ -1,15 +1,17 @@
-function generate_projects(user, slide_direction){
+function generate_projects(config, slide_direction){
     var css_class = class_slide(slide_direction);
-    console.log(css_class);
-    $.getJSON('https://api.github.com/users/' + user + '/repos?&sort=updated', function(resp) {
+    css_class = css_class ? css_class : '';
+    $.getJSON('https://api.github.com/users/' + config.user + '/repos?&sort=updated', function(resp) {
         if (resp.length > 0) {
             $('#github-gpg-repos').append('<ul id="project-gpg-list" class="unstyled"></ul>');
             $.each(resp, function(i, repositorio) {
-                var tag_name = '<h2 class="project-gpg-name">' + repositorio['name'] + '</h2>';
-                var tag_language = '<h3 class="project-gpg-language muted">' + ((repositorio['language']) ? repositorio['language'] : '') + '</h3>';
-                var tag_description = '<p class="project-gpg-description">' + ((repositorio['description']) ? repositorio['description'] : '') + '</p>';
-                var tag_li = '<li class="github-gpg-project github-gpg-sombra ' + css_class + ' well span3"><a href="' + repositorio['html_url'] + '">' + tag_name + tag_language + tag_description + '</a></li>';
-                $('#github-gpg-repos > ul').append(tag_li);
+                if($.inArray(repositorio['name'], config.exclude_list) == -1){
+                    var tag_name = '<h2 class="project-gpg-name">' + repositorio['name'] + '</h2>';
+                    var tag_language = '<h3 class="project-gpg-language muted">' + ((repositorio['language']) ? repositorio['language'] : '') + '</h3>';
+                    var tag_description = '<p class="project-gpg-description ' + (css_class ? 'project-gpg-description-caption' : '') +'">' + ((repositorio['description']) ? repositorio['description'] : '') + '</p>';
+                    var tag_li = '<li class="github-gpg-project github-gpg-sombra ' + css_class + (css_class ? ' github-gpg-project-slide' : '') +' well span3"><a href="' + repositorio['html_url'] + '">' + tag_name + tag_language + tag_description + '</a></li>';
+                    $('#github-gpg-repos > ul').append(tag_li);
+                }
             });
         }
         else {
@@ -19,7 +21,7 @@ function generate_projects(user, slide_direction){
 }
 
 function class_slide(slide_direction){
-    var css_class = 'github-gpg-cap-bottom';
+    var css_class = null;
     switch(slide_direction){
         case 'top': {
             css_class = 'github-gpg-cap-top';
@@ -45,10 +47,11 @@ function class_slide(slide_direction){
     $.GithubPortfolioGenerator = function(settings){
         var config = {
             'user': 'ArthurAssuncao',
-            'slide_direction' : 'bottom',
+            'slide_direction' : null,
+            'exclude_list' : new Array(),
         };
         if (settings){$.extend(config, settings);}
-        generate_projects(config.user, config.slide_direction);
+        generate_projects(config, config.slide_direction);
 
         return this;
     };
