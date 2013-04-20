e39136ee7060d218e71733a4f75a0954374a32c9
ArthurAssuncao <arthur.xx92@gmail.com>
2013-04-20 00:51:57 -0300

    atualizado: arquivos do github portfolio generator

--- github_portfolio_generator/github_portfolio_generator.js
+++ github_portfolio_generator/github_portfolio_generator.js
@@ -1,19 +1,23 @@
 $(document).ready(function() {
+    generate_projects();
+});
+
+function generate_projects(){
     $.getJSON('https://api.github.com/users/ArthurAssuncao/repos?&sort=updated', function(resp) {
         if (resp.length > 0) {
-            $('#github-repos').append('<ul id="project-list" class="unstyled"></ul>');
+            $('#github-gpg-repos').append('<ul id="project-gpg-list" class="unstyled"></ul>');
             var tags_li = '';
             $.each(resp, function(i, repositorio) {
-                var tag_name = '<h2 class="project-name">' + repositorio['name'] + '</h2>';
-                var tag_language = '<h3 class="project-language muted">' + ((repositorio['language']) ? repositorio['language'] : '') + '</h3>';
-                var tag_description = '<p class="project-description">' + ((repositorio['description']) ? repositorio['description'] : '') + '</p>';
-                var tag_li = '<li class="github-project sombra well span3"><a href="' + repositorio['html_url'] + '">' + tag_name + tag_language + tag_description + '</a></li>';
+                var tag_name = '<h2 class="project-gpg-name">' + repositorio['name'] + '</h2>';
+                var tag_language = '<h3 class="project-gpg-language muted">' + ((repositorio['language']) ? repositorio['language'] : '') + '</h3>';
+                var tag_description = '<p class="project-gpg-description">' + ((repositorio['description']) ? repositorio['description'] : '') + '</p>';
+                var tag_li = '<li class="github-gpg-project github-gpg-sombra well span3"><a href="' + repositorio['html_url'] + '">' + tag_name + tag_language + tag_description + '</a></li>';
                 tags_li += tag_li;
             });
-            $('#github-repos > ul').append(tags_li);
+            $('#github-gpg-repos > ul').append(tags_li);
         }
         else {
-            $('#github-repos').append('<p>No public repositories.</p>');
+            $('#github-gpg-repos').append('<p>No public repositories.</p>');
         }
     });
-});
+}
