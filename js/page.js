c1e0d4eaa1c4100964b45ff15e551e7f80c02767
ArthurAssuncao <arthur.xx92@gmail.com>
2013-05-04 19:17:09 -0300

    modificado: layout da pagina, iniciado versao 1.1 do layout

--- js/page.js
+++ js/page.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
-    get_numero_repositorios_publicos();
     get_numero_seguidores();
     get_repositorios_atualizados(3);
     get_organizations();
+    get_dados_estatisticas();
     add_tooltip();
+    add_scrollbar();
 });
 
 function get_numero_repositorios_publicos(){
@@ -22,16 +23,8 @@ function get_repositorios_atualizados(numero_repositorios){
     $.getJSON('https://api.github.com/users/ArthurAssuncao/repos?&sort=updated&per_page='+ numero_repositorios +'&page=1', function(resp) {
         if (resp.length > 0) {
             $('#recentemente_atualizados').append('<ul class="unstyled"></ul>');
-            //console.log($(resp.data).sort('pushed_at', 'desc'));
-            resp_ordenado = resp.sort(function(a, b) {
-               var compA = a['updated_at'];
-               var compB = b['updated_at'];
-               return (compA < compB) ? 1 : (compA > compB) ? -1 : 0;
-            });
             var tags_li = '';
-            var total_forks = 0;
-            var total_estrelas = 0;
-            $.each(resp_ordenado, function(i, repositorio) {
+            $.each(resp, function(i, repositorio) {
                 var tag_nome = '<a href="' + repositorio['html_url'] + '">' + repositorio['name'] + '</a>';
                 var tag_data = '<a class="muted" href="' + repositorio['html_url'] + '/commits">' + new Date(repositorio['updated_at']).toLocaleDateString() + '</a>';
                 var numero_forks = repositorio['forks_count'];
@@ -40,11 +33,8 @@ function get_repositorios_atualizados(numero_repositorios){
                 var tag_estrelas = '<a class="muted" href="' + repositorio['html_url'] + '/network">' + numero_estrelas + ' estrelas</a>';
                 var tag_li = '<li>' + tag_nome + ' <span class="muted">' + tag_data + ' ' + tag_forks + ' ' + tag_estrelas + '</span><li>';
                 tags_li += tag_li;
-                total_forks += numero_forks;
-                total_estrelas += numero_estrelas;
             });
             $('#recentemente_atualizados > ul').append(tags_li);
-            get_total_forks_estrelas(total_forks, total_estrelas);
         }
         else {
             $('#github-repos').append('<p>No public repositories.</p>');
@@ -52,19 +42,25 @@ function get_repositorios_atualizados(numero_repositorios){
     });
 }
 
-function get_total_forks_estrelas(total_forks, total_estrelas){
-    if(typeof total_forks == 'undefined' && typeof total_estrelas == 'undefined'){
-        $.getJSON('https://api.github.com/users/ArthurAssuncao/repos', function(resp) {
-            var total_forks = 0;
-            var total_estrelas = 0;
+function get_dados_estatisticas(){
+    $.getJSON('https://api.github.com/users/ArthurAssuncao/repos?&sort=updated', function(resp) {
+        var total_forks = 0;
+        var total_estrelas = 0;
+        var avatar_url = '';
+        if (resp.length > 0) {
             $.each(resp, function(i, repositorio) {
                 total_forks += repositorio['forks_count'];
                 total_estrelas += repositorio['watchers_count'];
+                if(avatar_url == '' && repositorio['owner']['login'] == 'ArthurAssuncao'){
+                    avatar_url = repositorio['owner']['avatar_url'];
+                }
             });
-        });
-    }
-    $('#total_forks').html(total_forks);
-    $('#total_estrelas').html(total_estrelas);
+        }
+        $('#total_forks').html(total_forks);
+        $('#total_estrelas').html(total_estrelas);
+        $('#repositorios_publicos').html(resp.length);
+        $('#avatar_usuario').attr('src', avatar_url);
+    });
 }
 
 function get_organizations(){
@@ -72,8 +68,6 @@ function get_organizations(){
         if (resp.length > 0) {
             $('#organizacoes').append('<h5>Organizações</h5>');
             $('#organizacoes').append('<ul class="unstyled inline"></ul>');
-            var total_forks = 0;
-            var total_estrelas = 0;
             var tags_li = new Array(resp.length);
             $.each(resp, function(i, organizacao) {
                 tags_li[i] = '<li><a href="' + organizacao['repos_url'] + '"><img class="organizacao-logo bootstrap-tooltip" src="' + organizacao['avatar_url'] + '" alt="logo da ' + organizacao['login'] + '" width="36px" height="36px" title="' + organizacao['login'] + '" /></a>';
@@ -86,4 +80,8 @@ function get_organizations(){
 
 function add_tooltip(){
     $('.bootstrap-tooltip').tooltip({placement : 'bottom'});
+}
+
+function add_scrollbar(){
+    $('#lado-esquerdo').jScrollPane();
 }
